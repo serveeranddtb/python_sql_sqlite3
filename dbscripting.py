a641104062665c2c0683bbc0cdda295c6ab420a2
ILYAS <server.and.dtb@gmail.com>
2024-02-24 16:37:49 +0100

    library to pass sql commands

---
+++ dbscripting.py
@@ -0,0 +1,12 @@
+import sqlite3 
+
+
+connection= sqlite3.connect('db.sqlite3')
+
+print("connecetd ....")
+
+cursor = connection.cursor()
+
+print("got the cursor , we can pass commands now ....")
+
+cursor.execute("CREATE TABLE AiModels(ID , name , type , generation)")
